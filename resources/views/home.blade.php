084578ebb986177ab3afde880be1dab0845c1a2b
hasnain <hasnainshoaib45@gmail.com>
2021-11-22 18:46:21 +0500

    Calculation on new expenses done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -38,47 +38,74 @@
                 </div>
                 <div class="row">
                     <div class="offset-md-11 col-md-1 mt-2">
-                        <i id="add-expense" class="far fa-plus-circle fa-lg text-success" style="cursor: pointer"></i>
+                        <i id="add-expense" class="far  fa-plus-circle fa-lg text-success" style="cursor: pointer"></i>
                     </div>
                 </div>
+
                 <hr class="text-white">
                 <x-forms.input-line title="Total" id='total' title-type="text-danger" :total='true' />
             </div>
         </div>
     </div>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 	<script src="{{asset('assets/js/jquery.js')}}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
     <script>
-        let food = 0;
-        let petrol = 0;
-        let expenses = 0;
         $(document).ready(function(){
-            // $('.expenses').on('change', function(){
-            //     expenses += parseInt($(this).val());
-            //     console.log(expenses);
-            //     $('#total').val(0);
-            //     $('#total').val(expenses);
-            // })
-            $('#add-expense').on('click', function(){
-                $('#expense-div').append(`<x-forms.input-line title="Food" id='food' :close='true' />`)
-            });
+            let expenses = 0;
+            $(document).on('change','.expenses', function(){
+                //  Setting expenses to zero before calculating each expense input field
+                expenses = 0;
+                
+                // Loop through all expenses input and add them to the expenses variable
+                $('.expenses').each((i, el) => {
+                    if(el.value){
+                        expenses += parseInt(el.value);
+                    }
+                });
+                $('#total').val(expenses);
+            })
 
             $(document).on('click','.close', function(){
+                $(this).parent().prev().html('');
                 $(this).parent().parent().addClass('d-none');
             })
 
-            $('#food').on('change', function(){
-                food = 0;
-                food = parseInt($(this).val());
-                expenses = food + petrol;
-                $('#total').val(0);
-                $('#total').val(expenses);
-            })
-            $('#petrol').on('change', function(){
-                petrol = 0;
-                petrol = parseInt($(this).val());
-                expenses = food + petrol;
-                $('#total').val(0);
-                $('#total').val(expenses);
+            $('#add-expense').on('click', function(){
+                Swal.fire({
+                title: 'Submit your Github username',
+                input: 'text',
+                inputAttributes: {
+                    autocapitalize: 'off'
+                },
+                showCancelButton: true,
+                confirmButtonText: 'Look up',
+                showLoaderOnConfirm: true,
+                preConfirm: (login) => {
+                    return fetch(`//api.github.com/users/${login}`)
+                    .then(response => {
+                        if (!response.ok) {
+                        throw new Error(response.statusText)
+                        }
+                        $('#expense-div').append(`<x-forms.input-line title="Food" id='food' :close='true' />`)
+
+                        return response.json()
+                    })
+                    .catch(error => {
+                        Swal.showValidationMessage(
+                        `Request failed: ${error}`
+                        )
+                    })
+                },
+                allowOutsideClick: () => !Swal.isLoading()
+                }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire({
+                    title: `${result.value.login}'s avatar`,
+                    imageUrl: result.value.avatar_url
+                    })
+                }
+                })
             })
         });
     </script>
